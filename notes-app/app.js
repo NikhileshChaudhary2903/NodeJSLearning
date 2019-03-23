c8e60b72517a6e38c93ec7af1b06109066be4f34
Nikhilesh Chaudhary <nikhileshchaudhary@Nikhileshs-MacBook-Pro.local>
2019-03-23 01:01:27 -0700

    changed

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,43 @@
 
-const validator = require('validator')
-console.log(validator.isEmail('nikhilchaudhary2903@gmail.com'))
-console.log(validator.isURL('nikhilchaudhary2903@/gmail.com'))
+// usage for validator module from npm
+// const validator = require('validator')
+// console.log(validator.isEmail('nikhilchaudhary2903@gmail.com'))
+// console.log(validator.isURL('nikhilchaudhary2903@/gmail.com'))
 
+
+
+
+// use npm init or npm install to load external dependencies into your project
+const mychalk = require('chalk')
+
+console.log(mychalk.red('Success'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// file write synchronous example
 // const fs = require('fs')
 // fs.writeFileSync('notes.txt','This is a sample text file');
+
+
+// sharing functions between files example
 // const shareThis = require('./utils.js') // ./ goes to root folder ie notes app
 // console.log(shareThis(5.09,6));
 
-// fs.appendFileSync('notes.txt',"\n this has been appended");
-const getNotes = require('./notes.js')
-// time to get notes
+// // fs.appendFileSync('notes.txt',"\n this has been appended");
+// const getNotes = require('./notes.js')
+// // time to get notes
 
-console.log(getNotes());
+// console.log(getNotes());
